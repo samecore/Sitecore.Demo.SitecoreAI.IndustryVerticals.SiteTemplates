929fbe80152b5bf79eef8021ccf8ea9236930518
Same <same@sitecore.net>
2026-02-15 13:47:55 +0400

    reverted:   industry-verticals/travel/src/hooks/useSearchTracking.ts

--- industry-verticals/travel/src/hooks/useSearchTracking.ts
+++ industry-verticals/travel/src/hooks/useSearchTracking.ts
@@ -51,7 +51,7 @@ export function useSearchTracking() {
         }
       });
 
-      router.push(url.replace('https://skywings.dev/', 'https://flynas-demo.vercel.app/'));
+      router.push(url);
     },
     [router]
   );
